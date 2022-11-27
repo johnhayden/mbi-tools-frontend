f1614c6ab5577323cb85a387c4a2770637d53821
John Hayden <john.hayden@watershedlrs.com>
2022-11-27 17:27:32 -0600

    Created basic front-end app including a simple component that displays mbi generate and validate functionality

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <head>
+    <title>Medicare Beneficiary Identifier Tools</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous">
+  </head>
+
+  <html lang="en">
+  <nav className="navbar navbar-light bg-light">
+    <a className="navbar-brand" href="/">Medicare Beneficiary Identifier Tools</a>
+  </nav>
+
+  <MbiTools/>
+
+  </html>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MbiTools from './components/MbiTools.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MbiTools
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MbiTools.vue
@@ -0,0 +1,94 @@
+<template>
+
+  <div class="container">
+
+  <div class="p-3">
+    <h6>MBI Generator</h6>
+    <button @click="generateMBI" class="btn btn-primary">Generate MBI</button>
+    <span id="generated-mbi" class="m-2 text-muted">{{ mbi }}</span>
+    <img v-if="mbi" @click="copyMbiToClipboard" src="../assets/copy.png" alt="Copy to clipboard" class="cursor-pointer" width="20" height="20" />
+  </div>
+
+  <div class="p-3">
+    <div class="form-group">
+      <label for="mbi-test">MBI Verifier</label>
+      <input :class="mbiVerifyResult && mbiVerifyResult.class" class="form-control" v-on:input="mbiVerifyResult = null"
+             type="text" id="mbi-test" name="mbi-test" v-model="mbiTest" placeholder="Enter MBI"/>
+    </div>
+    <button @click="verifyMBI" :disabled='!mbiTest' class="btn btn-primary">Verify MBI</button>
+    <span v-if="mbiVerifyResult" id="validation-result" class="m-2 text-muted">
+      {{ mbiVerifyResult.valid ? "This is a valid MBI" : "This is an invalid MBI" }}
+    </span>
+  </div>
+
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'MbiTools',
+  devServer: {
+    // This will forward any request that does not match a static file to localhost:3000
+    proxy: 'http://localhost:8080'
+  },
+  data() {
+    return {
+      mbi: '',
+      mbiTest: '',
+      mbiVerifyResultClass: '',
+      mbiVerifyResult: null
+    }
+  },
+  methods: {
+    generateMBI: function () {
+
+      const apiUrl = "http://localhost:8080";
+
+      axios.get(apiUrl + `/generate`)
+          .then(response => {
+            // JSON responses are automatically parsed.
+            this.mbi = response.data.result
+          })
+          .catch(e => {
+            console.error(e);
+            throw new Error('Server-side error generating MBI')
+          })
+    },
+
+    copyMbiToClipboard: function() {
+      navigator.clipboard.writeText(this.mbi).then(() => {
+        alert("Copied to clipboard");
+      });
+    },
+
+    verifyMBI: function () {
+
+      const apiUrl = "http://localhost:8080";
+
+      axios.post(apiUrl + `/verify`, {mbi: this.mbiTest})
+          .then(response => {
+            // JSON responses are automatically parsed.
+            console.log(response.data.valid);
+            this.mbiVerifyResult = response.data;
+            this.mbiVerifyResult.class = this.mbiVerifyResult.valid ? 'is-valid' : 'is-invalid';
+            console.log(this.mbiVerifyResult);
+          })
+          .catch(e => {
+            console.error(e);
+            throw new Error('Server-side error verifying MBI')
+          })
+    }
+  }
+
+}
+</script>
+
+<style>
+.cursor-pointer{
+  cursor: pointer;
+}
+</style>
+
