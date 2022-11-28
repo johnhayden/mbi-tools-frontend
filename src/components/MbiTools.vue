c18ab2c1134fc734907e2b64c1930da57c546311
John Hayden <john.hayden@watershedlrs.com>
2022-11-27 18:20:41 -0600

    cleaned out devServer bit

--- src/components/MbiTools.vue
+++ src/components/MbiTools.vue
@@ -30,10 +30,6 @@ import axios from 'axios'
 
 export default {
   name: 'MbiTools',
-  devServer: {
-    // This will forward any request that does not match a static file to localhost:3000
-    proxy: 'http://localhost:8080'
-  },
   data() {
     return {
       mbi: '',
